ea8728de53954b42111eff6d4caec61bd9565f3b
Matheus Martins <matheusvmm13@gmail.com>
2022-02-07 21:33:54 +0100

    created routes to pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import { Navigate, Route, Routes, Link } from "react-router-dom";
 import Filter from "./components/Filter/Filter";
+import FavoritesPage from "./pages/FavoritesPage/FavoritesPage";
+import EditPage from "./pages/EditPage/EditPage";
+import CreatePage from "./pages/CreatePage/CreatePage";
+import CharacterPage from "./pages/CharacterPage/CharacterPage";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
@@ -12,30 +18,25 @@ function App() {
             <div className="d-lg-inline-flex" id="navbarNav">
               <ul className="navbar-nav d-flex flex-row-reverse">
                 <li className="nav-item ms-2 me-2">
-                  <a
-                    className="nav-link active"
-                    aria-current="page"
-                    href="https://www.google.com/"
-                  >
-                    Favorites
-                  </a>
+                  <Link to="/favorites">Favorites</Link>
                 </li>
                 <li className="nav-item ms-2 me-2">
-                  <a
-                    className="nav-link active"
-                    aria-current="page"
-                    href="https://www.google.com/"
-                  >
-                    Create
-                  </a>
+                  <Link to="/create">Create</Link>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/edit/:id" element={<EditPage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/character/:id" element={<CharacterPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
         <Filter></Filter>
-
       </header>
 
       <section className="container">
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,9 @@
 const HomePage = () => {
-  return <></>;
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
 };
 
 export default HomePage;
