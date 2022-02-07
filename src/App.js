48be2c69a189096e02797dd88745e7bcb7efc75f
David Soto <davidsotosendarrubias@gmail.com>
2022-02-07 19:06:50 +0100

    added basic html structure to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,67 @@
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <header>
+        <div>
+          <img src="#" alt="" />
+          <a href="#">mainPage</a>
+        </div>
+        <ul>
+          <li>
+            <a href="#">create</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">favorite</a>
+          </li>
+        </ul>
+      </header>
+
+      <section>
+        <h2>CardList</h2>
+        <div className="container">
+          <div className="row align-items-start">
+            <div className="col">
+              fvkuvuyyu
+              <li>card</li>
+              <li>card</li>
+              <li>card</li>
+            </div>
+            <div className="col">
+              giuybukyuk
+              <li>card</li>
+              <li>card</li>
+              <li>card</li>
+            </div>
+
+            <div className="col">
+              gyouyvuvy
+              <li>card</li>
+              <li>card</li>
+              <li>card</li>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer>
+        <h3>footer</h3>
+        <div className="container">
+          <div className="row d-flex justify-content-center text-center">
+            <img src="#" alt="" />
+            <img src="#" alt="" />
+            <img src="#" alt="" />
+            <img src="#" alt="" />
+            <img src="#" alt="" />
+          </div>
+          <div className="row d-flex justify-content-center text-center">
+            <p>first footer text</p>
+          </div>
+          <div className="row d-flex justify-content-center text-center">
+            <p>second footer text</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
