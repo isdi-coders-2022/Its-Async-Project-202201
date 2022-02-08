f58c37916ed7666c0358f8d3a5edb1ff69cd2d71
Matheus Martins <matheusvmm13@gmail.com>
2022-02-08 19:18:48 +0100

    fixed jest app test with timout

--- src/App.test.js
+++ src/App.test.js
@@ -3,17 +3,22 @@ import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-test("App re-routes", () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+describe("Given an App component", () => {
+  describe("When it is clicked on the favorites link", () => {
+    test("Then it renders an empty banner on the favorites page", () => {
+      jest.setTimeout(10000);
+      render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      );
 
-  const link = screen.getByRole("link", { name: /favorites/i });
-  userEvent.click(link);
+      const link = screen.getByRole("link", { name: /favorites/i });
+      userEvent.click(link);
 
-  const title = screen.getByRole("banner", { name: "" });
+      const title = screen.getByRole("banner", { name: "" });
 
-  expect(title).toBeInTheDocument();
+      expect(title).toBeInTheDocument();
+    });
+  });
 });
