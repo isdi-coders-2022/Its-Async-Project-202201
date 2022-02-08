229e3afb6a4eea168510522cadd83036b6b43db7
Matheus Martins <matheusvmm13@gmail.com>
2022-02-08 19:25:31 +0100

    fixed eslint issue

--- src/App.test.js
+++ src/App.test.js
@@ -6,10 +6,8 @@ import App from "./App";
 describe("Given an App component", () => {
   describe("When it is clicked on the favorites link", () => {
     test("Then it renders an empty banner on the favorites page", () => {
-<<<<<<< HEAD
       jest.setTimeout(10000);
-=======
->>>>>>> 87a6766afee943b527c4c6fa6eb0374266ecc1a0
+
       render(
         <BrowserRouter>
           <App />
