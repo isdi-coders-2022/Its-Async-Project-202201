77c90cafe69ab95706fe4938e288b573226bd2d1
David Soto <davidsotosendarrubias@gmail.com>
2022-02-09 20:00:58 +0100

    added style to card component and margin to footer

---
+++ src/styles/CharacterCard.styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const CharacterCardStyled = styled.div`
+  .card {
+    background-color: #4d4f56;
+
+    &:hover {
+      background-color: #55cc44;
+
+      .favIcon {
+        display: block;
+        color: #f0eb74;
+        background-color: #24282f;
+      }
+    }
+  }
+  .linksContainer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .favIcon {
+    color: #4d4f56;
+    background-color: #24282f;
+    padding: 7px;
+    border-radius: 5px;
+  }
+`;
+
+export default CharacterCardStyled;
