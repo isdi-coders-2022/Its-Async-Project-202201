bb8c4c6e094a2687656d14910459f5d90bac9c89
Matheus Martins <matheusvmm13@gmail.com>
2022-02-10 13:41:25 +0100

    fixed eslint issues on Character Cards

--- src/components/CharacterDetailCard/CharacterDetailCard.js
+++ src/components/CharacterDetailCard/CharacterDetailCard.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import CharacterCardStyled from "../../styles/CharacterCard.styled";
 import FavIcon from "../Icon/FavIcon";
 
--- src/pages/CharacterPage/CharacterPage.js
+++ src/pages/CharacterPage/CharacterPage.js
@@ -1,12 +1,10 @@
 import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import CharacterCard from "../../components/CharacterCard/CharacterCard";
 import CharacterDetailCard from "../../components/CharacterDetailCard/CharacterDetailCard";
 import Filter from "../../components/Filter/Filter";
 import useCharacters from "../../hooks/useCharacters";
 import CharacterContext from "../../store/contexts/CharacterContext";
 import CardListStyled from "../../styles/CardList.styled";
-import CharacterCardStyled from "../../styles/CharacterCard.styled";
 
 const CharacterPage = () => {
   const { characters } = useContext(CharacterContext);
