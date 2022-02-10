baf75df6f98bd5d9950da1696b117d4fb87bd333
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-02-10 11:59:37 +0100

    homePage rendering test

---
+++ src/pages/HomePage/HomePage.test.js
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { nextPage } from "../../store/actions/characters/actionsCreators";
+import CharacterContextProvider from "../../store/contexts/CharacterContextProvider";
+import HomePage from "./HomePage";
+
+describe("Given a HomePage", () => {
+  describe("When its instancied", () => {
+    test("then it should render a heading ALL CHARACTERS", () => {
+      render(
+        <BrowserRouter>
+          <CharacterContextProvider>
+            <HomePage />
+          </CharacterContextProvider>
+        </BrowserRouter>
+      );
+
+      const expectedElement = screen.getByRole("heading", {
+        name: "ALL CHARACTERS",
+      });
+
+      expect(expectedElement).toBeInTheDocument();
+    });
+    test("then it should render 5 buttons", () => {
+      render(
+        <BrowserRouter>
+          <CharacterContextProvider>
+            <HomePage />
+          </CharacterContextProvider>
+        </BrowserRouter>
+      );
+
+      const items = screen.getAllByRole("button");
+
+      expect(items).toHaveLength(5);
+    });
+    test("then it should render 20 list items", () => {
+      render(
+        <BrowserRouter>
+          <CharacterContextProvider>
+            <HomePage />
+          </CharacterContextProvider>
+        </BrowserRouter>
+      );
+
+      const items = screen.getAllByRole("list");
+
+      expect(items).toHaveLength(1);
+    });
+  });
+});
