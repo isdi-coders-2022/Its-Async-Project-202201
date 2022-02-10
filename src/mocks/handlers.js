dcd015634bc177cd9d6e367bf1d3c7eb0a018f7e
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-02-10 18:54:14 +0100

    handlers modified

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,15 +1,11 @@
 import { rest } from "msw";
 export const handlers = [
   rest.get(process.env.REACT_APP_URLAPI, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx
-        .json
-        //el que ens ha de tornar la api, un objecte o llista de caracters, etc...)
-        ()
-    );
+    return res(ctx.status(200), ctx.json({ name: "rick" }));
   }),
 ];
+
+//el que ens ha de tornar la api, un objecte o llista de caracters, etc...)
 //manejar las peticiones de la api
 
 // si a jest senvia una peticio get i amb la rl enviada, ho pilla jest
